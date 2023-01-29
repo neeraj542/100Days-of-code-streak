f8fc68e5e902b228653bbe0c45a0840955fdbcd5
Neeraj Meena IND <114648043+neeraj542@users.noreply.github.com>
2023-01-30 02:53:42 +0530

    Add files via upload

---
+++ Day-1__(28-JAN-2023)/secondLargest.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void sortArray(int arr[], int size)
+{
+
+    for (int i = 0; i < size - 1; i++)
+    {
+
+        int minIndex = i;
+
+        for (int j = i + 1; j < size; i++)
+        {
+
+            if (arr[j] < arr[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+
+        swap(arr[minIndex], arr[i]);
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+int main()
+{
+    int arr[6] = {12, 35, 1, 10, 34, 1};
+
+    int size = 6;
+
+    sortArray(arr, 6);
+
+    return 0;
+}
---
+++ Day-1__(28-JAN-2023)/try.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    char ch = int(254);
+    cout << ch;
+
+
+    return 0;
+}
