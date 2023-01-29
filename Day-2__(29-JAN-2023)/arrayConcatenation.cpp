6fca28d58c5c6735e091759d203dd906b2164299
Neeraj Meena IND <114648043+neeraj542@users.noreply.github.com>
2023-01-30 02:54:09 +0530

    Add files via upload

---
+++ Day-2__(29-JAN-2023)/arrayConcatenation.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[5] = {12, 23, 34, 42, 52};
+    int brr[5] = {61, 72, 83, 94, 5};
+    // int crr[10] = {0};
+
+    for(int i=0; i<5; i++){
+        cout << arr[i] << " ";
+    }
+
+    for(int i=0; i<5; i++){
+        cout << brr[i] << " ";
+    }
+
+    return 0;
+}
