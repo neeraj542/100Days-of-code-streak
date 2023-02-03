0f2a088a5570856567351d71b2b3d3c851bd3c7f
Neeraj Meena IND <114648043+neeraj542@users.noreply.github.com>
2023-02-03 13:58:21 +0530

    Add files via upload

---
+++ Day-6__(02-FEB-2023)/try.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[6] = {2,5,1,3,4,7};
+    int n = 3;
+    int size = 6;
+
+        int resultArr[n*2];
+
+        for(int i=0; i<n; i++){
+
+            resultArr[2*i] = arr[i];
+            resultArr[2*i + 1] = arr[n+i];
+        }
+        // vector<int> result(resultArr, resultArr + (n*2));
+        for(int i=0; i<size; i++){
+            cout << resultArr[i] << " ";
+        }
+        
+}
