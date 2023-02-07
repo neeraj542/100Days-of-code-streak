dab227ed4e832cbb1925c5ba4e48879998958d1f
Neeraj Meena IND <114648043+neeraj542@users.noreply.github.com>
2023-02-07 21:37:12 +0530

    Add files via upload

---
+++ Day-10__(06-FEB-2023)/maxConsecutiveOnes.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int nums[6] = {1,1,0,1,1,1};
+    int max1 = 0;
+    int count = 0;
+    for (int i = 0; i < 6; i++)
+    {
+        if (nums[i] == 1)
+        {
+            count++;
+        }
+        else
+        {
+            max1 = max(max1, count);
+            count = 0;
+        }
+    }
+    max1 = max(max1, count);
+    
+    cout << max1 << endl;
+    return 0;
+}
---
+++ Day-10__(06-FEB-2023)/try.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s = "tour";
+    // string s;
+    // for(int i=0; i<s.length(); i++){
+    //     cin >> s[i];
+    // }
+
+    for(int i=0; i<s.length(); i++){
+        
+        if(i%2 != 0){
+            s[i] = '.';
+        }
+    }
+
+    for(int i=0; i<s.length(); i++){
+        cout << s[i];
+    }
+    return 0;
+}
