5af259edd92fa6fb156f40d9792ec35207387ca7
Neeraj Meena IND <114648043+neeraj542@users.noreply.github.com>
2023-02-09 23:41:34 +0530

    Add files via upload

---
+++ Day-11__(07-FEB-2023)/majorityElement.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{   
+        vector<int> nums= {2,2,1,1,1,2,2};
+        int size = nums.size();
+        int count = 0;
+
+        sort(nums.begin(),nums.end());
+
+        for(int i=0; i<size; i++){
+            
+            count = nums[(size-1)/2];
+        }
+
+        // return count;
+        cout << count << endl;
+    return 0;
+}
