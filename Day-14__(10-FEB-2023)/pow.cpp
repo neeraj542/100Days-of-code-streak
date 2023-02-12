18f8366f3bcb738a2e09b55b55126dbffa4faa91
Neeraj Meena IND <114648043+neeraj542@users.noreply.github.com>
2023-02-12 23:02:15 +0530

    Add files via upload

---
+++ Day-14__(10-FEB-2023)/pow.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double x = 2.10000;
+    // cin >> x;
+    int n = 3;
+    // cin >> n;
+
+    double pow1 = pow(x,n);
+
+    cout << "Result is: " << pow1 << endl;
+    
+    // return 0;
+}
---
+++ Day-14__(10-FEB-2023)/try.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double x;
+    cin >> x;
+    int n;
+    cin >> n;
+
+    double pow1 = pow(x,n);
+
+    cout << "Result is: " << endl;
+    
+    return 0;
+}
